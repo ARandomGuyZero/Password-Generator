b4f5d109e3dca4017c0c2eae42257cdd804360d3
Alan Ramos López <118926925+ARandomGuyZero@users.noreply.github.com>
2024-08-28 18:10:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 PyPassword Generator
 
 Author: Alan
-Date: August 21st 2024
+Date: August 23rd 2024
 
 This script generates a random password based on the user's preferences for the number of letters, symbols, and numbers. 
 The password is randomly shuffled to ensure security.
